cb3e16fcf766a9594a35fb6a99bcc35d12ba8b8a
Abdul Mulla <abdulmulla8071@gmail.com>
2026-08-18 17:44:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	const Fnames = document.querySelector('input[name="fname"]').value.trim()
+	const Lnames = document.querySelector('input[name ="lname"]').value.trim()
+	const sub = document.querySelector('input[type="submit"]');
+
+	alert(Fnames + " "+ Lnames)
+
+	
+
+
+	
+	
 
 }
